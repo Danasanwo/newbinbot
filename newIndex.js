14ed8ddf846b9c14a7b4c8fd4e1ccff602b5b0df
devdami <137655341+devdami@users.noreply.github.com>
2024-05-07 22:39:20 +0100

    12.8 commitment

--- newIndex.js
+++ newIndex.js
@@ -34,7 +34,7 @@ async function mainBot() {
         let getUSDTBalance = await (await binance.fetchBalance()).info.availableBalance
         let positionSymbols = allPositions.map(obj => obj.info.symbol)
         let uniquePositionSymbols = [...new Set(positionSymbols) ]
-        let numberOfAvailableOrders = 7 - uniquePositionSymbols.length 
+        let numberOfAvailableOrders = 10 - uniquePositionSymbols.length 
     
         for (pos of allPositions) {
             newRiskManager.setStopLossTakeProfit(pos, binance, symBolData)
