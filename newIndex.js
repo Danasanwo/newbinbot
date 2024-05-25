c2a9c10ed6714b4e93d5ab3a806ef43c317933b4
devdami <137655341+devdami@users.noreply.github.com>
2024-05-25 17:08:14 +0100

    15.5 commitment

--- newIndex.js
+++ newIndex.js
@@ -39,7 +39,7 @@ async function mainBot() {
         let getUSDTBalance = await (await binance.fetchBalance()).info.availableBalance
         let positionSymbols = allPositions.map(obj => obj.info.symbol)
         let uniquePositionSymbols = [...new Set(positionSymbols) ]
-        let numberOfAvailableOrders = 7 - uniquePositionSymbols.length 
+        let numberOfAvailableOrders = 4 - uniquePositionSymbols.length 
         let numberOfAvailableOrders80 = 10 - uniquePositionSymbols.length
     
 
